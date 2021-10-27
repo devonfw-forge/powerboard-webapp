3896f29642fca6a652c9b483214aed9af1c0470e
LANJEWAR <sakshi-sharad.lanjewar@capgemini.com>
2021-10-27 18:30:00 +0530

    end points fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
  
 
-  globalEndPoint : "http://test-api-lb-673763195.eu-central-1.elb.amazonaws.com/v1/",
+  globalEndPoint : "http://localhost:3001/v1/",
   
 };
 
