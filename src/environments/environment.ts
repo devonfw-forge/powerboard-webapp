231562a50ea66ef4f610c56b375490d90c31601a
Kurella <bhargavi.kurella@capgemini.com>
2022-08-23 13:51:42 +0530

    show widgets in dashboard when data is unavailable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
    production: false,
-   globalEndPoint: "http://localhost:3000/bapi/v1/",
-   AWS_ASSETS_URL: ""
+   globalEndPoint: "http://test-api-lb-673763195.eu-central-1.elb.amazonaws.com/bapi/v1/",
+   AWS_ASSETS_URL: "https://powerboard-test.s3.eu-central-1.amazonaws.com/uploads/assets/"
 
 };
