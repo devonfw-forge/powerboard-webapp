9f46d7cee35188681ca555054d0d3f8c5e988e32
Kurella <bhargavi.kurella@capgemini.com>
2022-08-24 10:35:44 +0530

    show widgets when data is not availabe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
    production: false,
-   globalEndPoint: "http://test-api-lb-673763195.eu-central-1.elb.amazonaws.com/bapi/v1/",
-   AWS_ASSETS_URL: "https://powerboard-test.s3.eu-central-1.amazonaws.com/uploads/assets/"
+   globalEndPoint: "",
+   AWS_ASSETS_URL: ""
 
 };
